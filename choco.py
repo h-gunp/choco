e3bab931ea82c4e42592849ea7fd95f2db45457e
ghdrjsvy0826 <hjshgp13@gmail.com>
2025-06-22 01:57:26 +0900

    두번째 수정

--- choco.py
+++ choco.py
@@ -7,7 +7,7 @@ db_config = {
     'host': '127.0.0.1',
     'port': 3306,
     'user': 'root', 
-    'password': '?h10720!',
+    'password': '패스워드입니다.',
     'db': 'board',
     'charset': 'utf8mb4'
 }
